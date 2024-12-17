f514debdd21bfc71a4277b02ff87305f961d6281
DaniilGordeev <gordeev.daniil.andreevic@gmail.com>
2024-12-17 11:35:10 +0500

    Castaway

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>Castaway</h1>
+Это мой первый сайт это первая попытка использования scss и Бэм<br><br><br>
+<img src="./Castaway.png"><br><br>
---
+++ script/script.js
@@ -0,0 +1,18 @@
+const year = 2023
+const number = 2100
+if (year === number && year < number) {
+    console.log('end 1')
+} else if (year === number || year < number){
+    console.log('end 2')
+} else{
+    console.log('end 3')
+}
+
+const result = 'Hello' && true && 5 && 0 && 10
+console.log(result)
+
+const message = false || 0 || '' || 'hello'
+console.log(message)
+
+const bol = 0
+console.log(!!0)
